90948e812e4e777c38181dcc36ee9a94298171bc
avln1104 <avln1104@gmail.com>
2025-03-23 19:21:45 +0500

    Fixed hosts

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn config.wsgi
+web: gunicorn --bind 0.0.0.0:$PORT myproject.wsgi
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^ilyo0+358#f%fw=!!^m$b7%twp2byv+bbuvf&_5kb_hurfuzy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app-name.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-production-9384.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
